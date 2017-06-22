f69e9595de3c1cd7584c4352bbdf2de7df101878
BrokerXu <BrokerXu@users.noreply.github.com>
2017-06-22 17:40:15 +0800

    first commit

---
+++ FigureView.js
@@ -0,0 +1,273 @@
+/**
+ * Created by BrokerXu on 2017/6/21.
+ */
+import React, {Component} from 'react';
+import {Image, View, ScrollView, Dimensions, TouchableHighlight} from 'react-native';
+import PropTypes from 'prop-types';
+
+
+const {width} = Dimensions.get('window');
+
+/***
+ * react-native 实现的轮播图
+ *
+ *  <FigureView
+ *      imgArr={imageData.data}
+ *      figureHeight={140}
+ *      figurePlace='center'
+ *      dotDefaultColor="white"
+ *      dotSelectedColor="red"
+ *      dotRadius={5}
+ *      dotMargin={5}
+ *      isHasDotBackground={true}
+ *      dotBackgroundHeight={30}
+ *      dotBackgroundColor="#55555599"
+ *      autoTime={2000}
+ *      isAutoPlay={true}
+ *      onPageClick={this.onPageClick}/>
+ *
+ */
+export default class FigureView extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            currentPage: 0
+        };
+
+    }
+
+//设置props值类型
+    static propTypes = {
+        imgArr: PropTypes.array,
+        figureHeight: PropTypes.number,
+        figurePlace: PropTypes.string,
+        dotDefaultColor: PropTypes.string,
+        dotSelectedColor: PropTypes.string,
+        dotRadius: PropTypes.number,
+        dotMargin: PropTypes.number,
+        isHasDotBackground: PropTypes.bool,
+        dotBackgroundHeight: PropTypes.number,
+        dotBackgroundColor: PropTypes.string,
+        isAutoPlay: PropTypes.bool,
+        autoTime: PropTypes.number,
+        onPageClick: PropTypes.func,
+    };
+
+//初始化默认值
+    static defaultProps = {
+        //图片集合
+        imgArr: [],
+        // //焦点图的高度
+        figureHeight: 120,
+        // //焦点显示的位置{left,center,right}
+        figurePlace: 'left',
+        // //焦点默认状态的颜色
+        dotDefaultColor: 'white',
+        // //焦点选中后的颜色
+        dotSelectedColor: 'red',
+        // //焦点的半径
+        dotRadius: 10,
+        //焦点之间的间距
+        dotMargin: 10,
+        //是否显示底部背景
+        isHasDotBackground: true,
+        //底部背景的高度
+        dotBackgroundHeight: 35,
+        //显示的底部背景颜色
+        dotBackgroundColor: '#f7f7f7',
+        // //是否自动轮播
+        isAutoPlay: true,
+        // //轮播时间间隔(单位：ms)
+        autoTime: 2000,
+        onPageClick: (index) => {
+            console.log(index)
+        }
+    };
+
+
+    componentWillMount() {
+        let figurePlace = this.props.figurePlace;
+        this.dotJustifyContent = 'flex-start';
+        if ("left".startsWith(figurePlace)) {
+            this.dotJustifyContent = "flex-start";
+        } else if ('center'.startsWith(figurePlace)) {
+            this.dotJustifyContent = 'center';
+        } else if ('right'.startsWith(figurePlace)) {
+            this.dotJustifyContent = 'flex-end';
+        }
+    }
+
+    //开启定时器
+    startTimer() {
+        if (this.props.isAutoPlay) {
+            const pageCount = this.props.imgArr.length;
+            const autoTime = this.props.autoTime;
+            this.timer = setInterval(
+                () => {
+                    let currentPage = this.state.currentPage;
+                    let activePage = 0;
+                    if (currentPage + 1 >= pageCount) {
+                        activePage = 0;
+                    } else {
+                        activePage = currentPage + 1;
+                    }
+                    this.setState({
+                        currentPage: activePage
+                    });
+
+                    let offsetX = activePage * width;
+
+                    this.scrollFigure.scrollTo({x: offsetX, y: 0, animated: true});
+                },
+                autoTime
+            );
+        }
+    }
+
+    // //实现拖拽无限滚动
+    // scrollUnlimited(e) {
+    //    let offsetX = e.nativeEvent.contentOffset.x;
+    //    let pageCount = this.props.imgArr.length;
+    //    let maxOffsetX = pageCount * width;
+    //
+    //    if(offsetX>maxOffsetX){
+    //        this.setState({
+    //            currentPage:0
+    //        });
+    //        this.scrollFigure.scrollTo({x:0,y:0,animated:true});
+    //    }else if(offsetX<0){
+    //        this.setState({
+    //            currentPage:pageCount-1
+    //        });
+    //        this.scrollFigure.scrollTo({x:maxOffsetX,y:0,animated:true});
+    //    }
+    // }
+
+
+    //组件加载完毕
+    componentDidMount() {
+        //开启定时器
+        this.startTimer();
+
+    }
+
+    //组件将被销毁
+    componentWillUnmount() {
+        //清除定时器
+        this.timer && clearInterval(this.timer);
+    }
+
+
+    //开始拖拽时清除定时器
+    onScrollBeginDrag() {
+        //清除定时器
+        this.timer && clearInterval(this.timer);
+    }
+
+    //结束拖拽时开启定时器
+    onScrollEndDrag() {
+        //开启定时器
+        this.startTimer();
+    }
+
+
+    scrollNextPage(e) {
+        //获取偏移量
+        let offsetX = e.nativeEvent.contentOffset.x;
+
+        //得到当前页索引
+        let currentPage = Math.floor(offsetX / width);
+        //更新状态，刷新UI
+        this.setState({currentPage});
+
+    }
+
+    //点击原点切换页面
+    dotOnScroll(index) {
+        let offsetX = width * index;
+        this.setState({currentPage: index});
+        this.scrollFigure.scrollTo({x: offsetX, y: 0, animated: true});
+    }
+
+
+    render() {
+        return (
+            <View style={{height: this.props.figureHeight}}>
+                <ScrollView
+                    ref={(scrollFigure) => {
+                        this.scrollFigure = scrollFigure;
+                    }}
+                    horizontal={true}
+                    showsHorizontalScrollIndicator={false}
+                    pagingEnabled={true}
+                    sendMomentumEvents={true}
+                    // onScroll={(e) => this.scrollUnlimited(e)}
+                    //一帧滚动结束的时候调用
+                    onMomentumScrollEnd={(e) => this.scrollNextPage(e)}
+                    //开始拖拽
+                    onScrollBeginDrag={this.onScrollBeginDrag.bind(this)}
+                    //结束拖拽
+                    onScrollEndDrag={this.onScrollEndDrag.bind(this)}
+
+                >
+                    {
+                        this.props.imgArr.map((value, index) => {
+                            return <TouchableHighlight
+                                key={index}
+                                activeOpacity={0.98}
+                                onPress={() => this.props.onPageClick && this.props.onPageClick(index)}
+                            >
+                                <Image
+                                    key={index}
+                                    source={{uri: value.img}}
+                                    resizeMode="cover"
+                                    style={{height: this.props.figureHeight, width: width}}
+                                />
+                            </TouchableHighlight>
+                        })
+                    }
+
+                </ScrollView>
+                <View style={{
+                    width: width,
+                    height: this.props.dotBackgroundHeight,
+                    backgroundColor: this.props.isHasDotBackground ? this.props.dotBackgroundColor : 'translate',
+                    position: 'absolute',
+                    bottom: 0,
+                    flexDirection: 'row',
+                    justifyContent: this.dotJustifyContent
+                }}>
+                    <View style={{
+                        height: this.props.dotBackgroundHeight,
+                        flexDirection: 'row',
+                        alignItems: 'center',
+                        marginLeft: this.props.dotMargin,
+                        marginRight: this.props.dotMargin
+                    }}>
+                        {this.props.imgArr.map((value, index) => {
+                            return <TouchableHighlight
+                                key={index}
+                                activeOpacity={0.5}
+                                onPress={() => this.dotOnScroll(index)}
+                            >
+                                <View
+                                    key={index}
+                                    style={{
+                                        width: this.props.dotRadius * 2,
+                                        height: this.props.dotRadius * 2,
+                                        borderRadius: this.props.dotRadius,
+                                        backgroundColor: this.state.currentPage === index ? this.props.dotSelectedColor : this.props.dotDefaultColor,
+                                        marginLeft: this.props.dotMargin,
+                                    }}
+
+                                />
+                            </TouchableHighlight>;
+                        })}
+                    </View>
+                </View>
+            </View>
+        );
+    }
+}
---
+++ index.ios.js
@@ -0,0 +1,65 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+import FigureView from './FigureView';
+import imageData from './ImageData.json';
+
+
+export default class FigureViewMain extends Component {
+
+
+  onPageClick(index){
+
+    console.log("点击了".concat(index));
+
+  }
+
+
+  render() {
+    return (
+      <View style={styles.container}>
+       <FigureView
+           imgArr={imageData.data}
+           figureHeight={140}
+           figurePlace='center'
+           dotDefaultColor="white"
+           dotSelectedColor="red"
+           dotRadius={5}
+           dotMargin={5}
+           isHasDotBackground={true}
+           dotBackgroundHeight={30}
+           dotBackgroundColor="#55555599"
+           autoTime={2000}
+           isAutoPlay={true}
+           onPageClick={this.onPageClick}
+       />
+
+      </View>
+    );
+  }
+}
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    // justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+      paddingTop:30
+  }
+});
+
+AppRegistry.registerComponent('FigureView', () => FigureViewMain);
